2a0ac80da3f5fc973544ba028296031be7d962b2
Pawel Tutakiewicz <paw.j.tuta@gmail.com>
2022-12-04 19:12:46 +0100

    finish

--- word-count/WordCount/src/main/java/wordcount/Main.java
+++ word-count/WordCount/src/main/java/wordcount/Main.java
@@ -3,7 +3,11 @@ package wordcount;
 import com.google.common.collect.ImmutableList;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.stream.*;
 
@@ -35,6 +39,7 @@ public class Main {
         ImmutableList<String> files = new ImmutableList.Builder<String>().build();
 
         directories = getAllSubFolders(startFile);
+
         for (String dir:
              directories) {
             files = new ImmutableList.Builder<String>()
@@ -42,17 +47,67 @@ public class Main {
                     .addAll(getAllFilesFromDirectory(dir))
                     .build();
         }
-
+        System.out.println("Wait...");
         Map<String, List<String>> librarySensitive = new HashMap<>();
         Map<String, List<String>> libraryInsensitive = new HashMap<>();
         files.stream().forEach(f -> {
             librarySensitive.put(f,getWordsFromString(readTextFromFile(f)));
         });
-        libraryInsensitive.forEach((k,v) -> {
-            List<String> words = new ArrayList<>();
-            v.forEach(word -> words.add(word.toLowerCase()));
-            libraryInsensitive.put(k,words);
+        librarySensitive.forEach((k,v) -> {
+            libraryInsensitive.put(k, v.stream().map(word -> word.toLowerCase()).collect(Collectors.toList()));
         });
+        System.out.println("Wait a little bit longer...");
+        Map<String, Map<String,Long>>mapSens = new HashMap<>();
+        Map<String, Map<String,Long>>mapInsens = new HashMap<>();
+
+        librarySensitive.forEach((k,v) -> {
+            mapSens.put(k,reduce(v));
+        });
+
+        System.out.println("Almost there...");
+        libraryInsensitive.forEach((k,v) -> {
+            mapInsens.put(k,reduce(v));
+        });
+
+        System.out.println("Result: ");
+        finishUp(mapSens,"sens.txt");
+        finishUp(mapInsens, "insens.txt");
+    }
+
+    public static boolean finishUp(Map<String, Map<String,Long>> map, String name){
+        map.forEach((k,v) -> {
+            List<Map.Entry<String, Long>> entries = new ArrayList<>(v.entrySet());
+            Collections.sort(entries, (o1, o2) -> o1.getValue().compareTo(o2.getValue()));
+            writeResults(entries.toString(), name);
+        });
+        return true;
+    }
+
+    public static boolean writeResults(String content, String name){
+        final Path path = Paths.get("./out/data/"+name);
+        try (
+                final BufferedWriter writer = Files.newBufferedWriter(path,
+                        StandardCharsets.UTF_8, StandardOpenOption.CREATE);
+        ) {
+            writer.write(content);
+            writer.flush();
+        } catch (IOException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static long count(String word, List<String> words){
+        return words.stream().filter( w -> w.equals(word)).count();
+    }
+
+    public static Map<String, Long> reduce(List<String> list){
+        Map<String, Long> tmpMap = new HashMap<>();
+        Set<String> tmpSet = list.stream().collect(Collectors.toSet());
+        tmpSet.forEach( word -> tmpMap.put(word, count(word, list)));
+
+        return tmpMap;
     }
 
     public static String readTextFromFile(final String f) {
